a366b01e7c42a58f32fcb5fa8871595a1deae899
Harini Kannan <hkannan@mit.edu>
2014-07-20 04:07:34 -0700

    fixed flip

--- public/js/sense.js
+++ public/js/sense.js
@@ -289,63 +289,6 @@
     }
   };
 
-  Sense.prototype.toss = function() {
-    var intervalExpired = false;
-    var z_accels = [];
-    var x_accels = [];
-    var y_accels = [];
-
-    var Z_THRESHOLD = -1.5;
-    var X_Y_THRESHOLD = 2;
-
-    setInterval(function(){intervalExpired = true}, 250);
-
-    if (window.DeviceMotionEvent) {
-      var callback,
-          options = {
-
-          },
-          args = getArgs(arguments, options);
-
-      callback = args.callback;
-      options = args.options;
-
-      window.addEventListener('devicemotion', function (eventData) {
-        var acceleration = eventData.acceleration;
-
-        if (intervalExpired) {
-          z_accels[z_accels.length] = Math.round(10*acceleration.z)/10;
-          x_accels[x_accels.length] = Math.round(10*acceleration.x)/10;
-          y_accels[y_accels.length] = Math.round(10*acceleration.y)/10;
-
-          intervalExpired = false;
-          var zMove = false;
-          var xMove = true;
-          var yMove = true;
-          if (z_accels[z_accels.length-1] < Z_THRESHOLD || z_accels[z_accels.length-2] < Z_THRESHOLD) {
-            zMove = true;
-          }
-          if (Math.abs(x_accels[x_accels.length-1]) > X_Y_THRESHOLD) {
-            xMove = false;
-          }
-          if (Math.abs(y_accels[y_accels.length-1]) > X_Y_THRESHOLD) {
-            yMove = false;
-          }
-
-          if (zMove && !xMove && !yMove) {
-            document.getElementById("Toss").innerHTML = z_accels[z_accels.length - 1];
-            document.getElementById("TossAngle").innerHTML = "TOSS";
-
-          }
-            callback({
-              magnitude: z_accels[z_accels.length-1] > z_accels[z_accels.length-2] ? z_accels[z_accels.length-1] : z_accels[z_accels.length-2]
-            });
-
-        }
-      });
-    }
-  };
-
   Sense.prototype.flip = function() {
     var gammas = [];
     if (window.DeviceOrientationEvent) {
@@ -354,8 +297,7 @@
             gestureDuration: 250
           },
           args = getArgs(arguments, options),
-          lastSample,
-          intervalExpired = true;
+          intervalExpired = false;
 
       callback = args.callback;
       options = args.options;
@@ -367,11 +309,12 @@
         var found = false;
         if(intervalExpired) {
           gammas[gammas.length] = eventData.gamma;
-          for (var i=0; i < 5; i++) {
+          for (var i=1; i < 5; i++) {
             if (Math.abs(gammas[gammas.length-1] - gammas[gammas.length-1-i]) > 160) {
               document.getElementById("Flip").innerHTML = "FLIP";
               found = true;
               final_gamma = gammas[gammas.length - 1];
+              gammas = [];
               break;
             }
           }
